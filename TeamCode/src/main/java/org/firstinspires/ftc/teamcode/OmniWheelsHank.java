01784e72f78ad550c72fb2a54b8c37a52328d3c5
BiggieCh33se52 <hae717@outlook.com>
2024-10-21 14:08:56 -0700

    Update OmniWheelsHank.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmniWheelsHank.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmniWheelsHank.java
@@ -80,7 +80,7 @@ public class OmniWheels extends LinearOpMode {
     private Servo hand = null;
 
     @Override
-    public void runOpMode() {
+    public void runOpMode() }
 
         // Initialize the hardware variables. Note that the strings used here must correspond
         // to the names assigned during the robot configuration step on the DS or RC devices.
